21d1586285d40fb0ecf89a09488a7a7295f6c34a
Laura Szymańska <lauraszy@wp.pl>
2025-04-04 19:11:24 +0200

    portfolio page implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from '../views/homepage/homepage.component';
+import { PortfolioComponent } from '../views/portfolio/portfolio.component';
 
 
 export const routes: Routes = [
     {
         path: '',
-        
-        component: HomepageComponent
+        component: HomepageComponent,
+    },          
+    {
+        path: 'portfolio',
+        component: PortfolioComponent
     }
 ];
