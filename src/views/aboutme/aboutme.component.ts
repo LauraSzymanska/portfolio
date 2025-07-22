4728616787472109acc7a54e6b201b2b1fec8f9b
Laura Szymańska <lauraszy@wp.pl>
2025-07-22 14:27:18 +0200

    education section

--- src/views/aboutme/aboutme.component.ts
+++ src/views/aboutme/aboutme.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-aboutme',
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './aboutme.component.html',
   styleUrl: './aboutme.component.scss'
 })
 export class AboutmeComponent {
+
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
+  
 
 }
